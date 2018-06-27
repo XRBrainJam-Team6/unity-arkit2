0d97526d083e53b327a82582dabd638e24b65e0e
Saul_Pena <sapenagamero@gmail.com>
2018-06-27 11:32:29 -0400

    Head fading implementation

---
+++ Assets/Scripts/FadingPiece.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadingPiece : MonoBehaviour {
+
+    public transformation tr;
+    public int numberToStartAnimation;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (numberToStartAnimation == 1)
+        {
+            numberToStartAnimation = 2;
+            finishFading();
+        }
+	}
+
+    public void finishFading()
+    {
+        tr.pieceFinishedFading();
+    }
+
+    public void finishReverseFading()
+    {
+
+    }
+}
--- Assets/Scripts/transformation.cs
+++ Assets/Scripts/transformation.cs
@@ -5,55 +5,162 @@ using UnityEngine.UI;
 
 public class transformation : MonoBehaviour
 {
-
-    public GameObject successmarker;
-    public Renderer objectRenderer;
-    public Material mat1;
-    public Material mat2;
     public List<GameObject> piecesOfMask;
     public float numberOfSecondsLookingAtEyes;
     public float numberOfSecondContinous = 0;
     public bool lookingAtEyes;
     public Text counterTextUI;
-    public GameObject head;
-    public float indexOfLastPiece;
+    public int indexOfLastPiece;
     public ParticleSystem particleSystemWhenPieceIsGone;
+    public bool fading = false;
+    public bool initialFade = false;
+
 
     // Use this for initialization
     void Start()
     {
-        objectRenderer = successmarker.GetComponent<Renderer>();
-        receiveHeadMesh(head);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (lookingAtEyes)
+        if (Input.GetKeyDown("space"))
         {
-            
+            //nextPieceDissapearEvent();
+            startFadingHead();
+            print("space key was pressed");
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            //nextPieceDissapearEvent();
+            //startFadingHead();
+
+            print("space key was pressed");
+        }
+
+        if (Input.GetKeyDown("space"))
+        {
+            //nextPieceDissapearEvent();
+            //startFadingHead();
+            print("space key was pressed");
+        }
+
+
+        if (lookingAtEyes)
+        {   
             numberOfSecondContinous = numberOfSecondContinous + Time.deltaTime;
             counterTextUI.text = numberOfSecondContinous + "";
+            if (numberOfSecondContinous > 8 && initialFade == false)
+            {
+                startFadingHead();
+                initialFade = true;
+            }
         }
+
     }
 
-    public void changeRedMaterial()
+    public void startFadingHead()
     {
-        objectRenderer.material = mat2;
-    }
-
-    public void changeWhiteMaterial()
-    {
-        objectRenderer.material = mat1;
-    }
-
-    public void receiveHeadMesh(GameObject head )
-    {
-        foreach (Transform child in head.transform)
+        fading = true;
+        if (indexOfLastPiece < piecesOfMask.Count)
         {
-            piecesOfMask.Add(child.gameObject);
+            activateFadeAnimation(piecesOfMask[indexOfLastPiece]);
+            indexOfLastPiece++;
+            particleSystemWhenPieceIsGone.Clear();
+            particleSystemWhenPieceIsGone.Play();
         }
+    }
+
+    public void reverseFadingAllHead()
+    {
+        for (int i = 0; i < piecesOfMask.Count; i++)
+        {
+           // Animator anim = piecesOfMask[i].GetComponent<Animator>();
+            PauseSingleAnimation(piecesOfMask[i]);
+        }
+
+    }
+
+    public void resumePlayingInSameDirectionHead()
+    {
+        for (int i = 0; i < piecesOfMask.Count; i++)
+        {
+            if (fading)
+            {
+                Animator anim = piecesOfMask[i].GetComponent<Animator>();
+                anim.SetFloat("Speed", 1f);
+            }
+            else
+            {
+                Animator anim = piecesOfMask[i].GetComponent<Animator>();
+                anim.SetFloat("Speed", -1f);
+            }
+            //PauseSingleAnimation(piecesOfMask[i]);
+        }
+    }
+
+    public void activateFadeAnimation(GameObject piece)
+    {
+        Animator anim = piece.GetComponent<Animator>();
+        anim.SetFloat("Speed",1f);
+        anim.SetBool("Fade", true);
+        //piece.SetActive(false);
+    }
+
+    public void PauseSingleAnimation(GameObject piece)
+    {
+        Animator anim = piece.GetComponent<Animator>();
+        anim.SetFloat("Speed", 0f);
+        //anim.SetBool("Fade", true);
+    }
+
+    public void resverseFadeAnimation(GameObject piece)
+    {
+        //Animator anim = piece.GetComponent<Animator>();
+        //anim.SetBool("fade", false);
+        piece.SetActive(true);
     }
     
+
+    public void nextPieceDissapearEvent()
+    {
+        if (indexOfLastPiece < piecesOfMask.Count)
+        {
+            activateFadeAnimation(piecesOfMask[indexOfLastPiece]);
+            indexOfLastPiece++;
+            particleSystemWhenPieceIsGone.Clear();
+            particleSystemWhenPieceIsGone.Play();
+        }
+    }
+
+    public void revertPieceDissapearEvent()
+    {
+        if (indexOfLastPiece > 0)
+        {
+            resverseFadeAnimation(piecesOfMask[indexOfLastPiece]);
+            indexOfLastPiece--;
+        }
+    }
+
+    public void pauseAnimations()
+    {
+
+    }
+
+    public void pieceFinishedFading()
+    {
+        if (fading == true)
+        {
+            if (indexOfLastPiece < piecesOfMask.Count)
+            {
+                activateFadeAnimation(piecesOfMask[indexOfLastPiece]);
+                indexOfLastPiece++;
+                particleSystemWhenPieceIsGone.Clear();
+                particleSystemWhenPieceIsGone.Play();
+            }
+        }
+    }
 
 }
